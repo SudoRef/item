8a767906f9b42fe29cb1f99067a5b5b0218b2ed9
sudarev <sudarevri@gmail.com>
2022-05-20 22:24:16 +0200

    add tests

--- src/main/java/product/Basket.java
+++ src/main/java/product/Basket.java
@@ -1,12 +1,18 @@
 package product;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Basket {
 
     Set<Product> productList;
--- src/main/java/utils/ProductTaxCalculator.java
+++ src/main/java/utils/ProductTaxCalculator.java
@@ -8,10 +8,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class ProductTaxCalculator {
 
@@ -32,31 +29,52 @@ public class ProductTaxCalculator {
         Basket basket = new Basket();
         basket.setProductList(new HashSet<>(productList));
         basket.setSalesTaxes(calculateTaxes(productList));
-        basket.setTotal(calculateTotalPrice(basket.getSalesTaxes(), productList));
+        basket.setTotal(calculateTotalPrice(productList));
         return basket;
     }
-    private Double calculateTaxes(List<Product> products){
+
+    private Double calculateTaxes(List<Product> products) {
         double tax = 0.0;
-        for (Product product: products) {
-            if(product.getIsImported()){
-                tax+=product.getPrice()*0.05;
+        for (Product product : products) {
+            double productTax = 0.0;
+            if (product.getIsImported()) {
+                productTax += product.getPrice() * 0.05;
             }
-            if(Type.OTHER.equals(product.getType())){
-                tax+=product.getPrice()*0.1;
+            if (Type.OTHER.equals(product.getType())) {
+                productTax += product.getPrice() * 0.1;
+
             }
+            tax += productTax;
+            double scale = Math.pow(10, 2);
+            product.setPrice(Math.round((product.getPrice() + productTax) * scale) / scale);
+            updatePrice(product);
         }
 
         return Math.round(tax * 20.0) / 20.0;
     }
 
-    private Double calculateTotalPrice(Double tax, List<Product> products) {
+    private void updatePrice(Product product) {
+        String[] title = product.getTitle().split(" ");
+        title[title.length - 1] = String.valueOf(product.getPrice());
+        StringBuilder sb = new StringBuilder();
+        for (String str : title) {
+            if ("at".equals(str)) {
+                sb.append(":");
+                continue;
+            }
+            sb.append(" ").append(str);
+        }
+        product.setTitle(sb.toString().trim());
+    }
+
+    private Double calculateTotalPrice(List<Product> products) {
         Double totalPrice = 0.0;
         for (Product product : products) {
             totalPrice += product.getPrice();
         }
-        totalPrice += tax;
+
         return BigDecimal.valueOf(totalPrice)
-                .round( new MathContext(4, RoundingMode.UP) )
+                .round(new MathContext(4, RoundingMode.UP))
                 .doubleValue();
     }
 
